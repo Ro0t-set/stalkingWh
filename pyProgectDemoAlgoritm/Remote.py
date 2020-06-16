91dc97818bf68cea9f5439e5617cb15eefbb631a
Ro0t-set <ro0trexustom@gmail.com>
2020-06-16 18:00:45 +0200

    pulizia

--- pyProgectDemoAlgoritm/Remote.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Remote(
-    desired_capabilities=webdriver.DesiredCapabilities.CHROME,
-    command_executor='http://127.0.0.1:4444/wd/hub')
-driver.get('http://www.google.com')
